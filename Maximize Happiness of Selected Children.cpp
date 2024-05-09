cc364029c5cc810e24a02bdf0754074b7c71125e
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2024-05-09 23:00:29 +0600

    Create Maximize Happiness of Selected Children.cpp

---
+++ Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort (happiness.begin(), happiness.end(), greater <int> ());
+
+        long long int ans = 0;
+        for (int i = 0; i < k; i++) {
+            ans += max(0, happiness[i] - i);
+        }
+
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/maximize-happiness-of-selected-children/description
