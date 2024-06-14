7aa1832c6495e566429761d580a395d345a4789a
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2024-06-14 13:44:16 +0600

    Create 945. Minimum Increment to Make Array Unique.cpp

---
+++ 945. Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort (nums.begin(), nums.end());
+
+        int moves = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            while (nums[i] >= nums[i + 1]) {
+                nums[i + 1]++;
+                moves++;
+            }
+        }
+
+        return moves;
+    }
+};
+
+// https://leetcode.com/problems/minimum-increment-to-make-array-unique/
