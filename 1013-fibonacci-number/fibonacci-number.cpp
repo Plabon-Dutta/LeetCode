d6057dff545aaa0b501a66721324469faf2f3f03
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2025-04-07 09:41:30 +0600

    Time: 14 ms (22.32%) | Memory: 7.6 MB (87.52%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    int fib2(int i, int j, int c, int n) {
-        if (c == n) return i + j;
-        return fib2(j, i + j, c + 1, n);
-    }
-
     int fib(int n) {
         if (n <= 1) return n;
-        
-        return fib2(0, 1, 2, n);
+        return fib(n - 1) + fib(n - 2);
     }
 };
